29133473c7b43f3e4f25d11db5851d7c83bd39ae
Dipraj Howlader <diprajhowlader2015@gmail.com>
2022-02-09 19:26:43 +0600

    finally done

--- Expression.java
+++ Expression.java
@@ -6,18 +6,32 @@ public class Expression {
 	public static void main(String[] args) {
 		Scanner inScanner = new Scanner(System.in);
 		int[] digits = new int[3];
-		int[] cal = new int[4];
+		int[] cal = new int[8];
 	
-		for(int i=0;i<3;i++)
-			digits[i] = inScanner.nextInt();
-			Arrays.sort(digits);
+		for(int i=0;i<3;i++){
+			int x=inScanner.nextInt();
+			if(x>=1 && x<=10)
+			digits[i] = x;
+		}
 		cal[0]=digits[0]+digits[1]*digits[2];
+
 		cal[1]=digits[0]*(digits[1]+digits[2]);
+
 		cal[2]=digits[0]*digits[1]*digits[2];
+
 		cal[3]=(digits[0]+digits[1])*digits[2];
+
+		cal[4] = digits[0]*digits[1]+digits[2];
+
+		cal[5] = digits[0]+digits[1]+digits[2];
+
+		cal[6] = digits[0]+(digits[1]*digits[2]);
+
+		cal[7] = (digits[0]*digits[1])+digits[2];
+
 		int max=cal[0];
 		for (int i = 1; i < cal.length; i++)
-            if (cal[i] > max)
+            if (cal[i] >= max)
                 max = cal[i];
 		
 			System.out.print(max);
