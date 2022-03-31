0f6f44118579756fcf078b9610713ee988ce7fda
Dipraj Howlader <diprajhowlader2015@gmail.com>
2022-03-31 22:23:49 +0600

    well contest

---
+++ A.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class A {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        int test = iScanner.nextInt();
+        while(test>0){
+            test--;
+            String str = iScanner.next();
+            int count =0;
+        }
+    }
+}
--- check.java
+++ check.java
@@ -1,8 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class check {
     public static void main(String[] args) {
-    
-            int a =6;
-            int res = (int) (Math.log(a) / Math.log(2));
-            System.out.println(res);
+        Scanner iScanner = new Scanner(System.in);
+        int test = iScanner.nextInt();
+        while(test>0){
+            test--;
+            int n = iScanner.nextInt();
+            long[] arr = new long[n];
+            for(int i=0;i<n;i++)
+            arr[i] = iScanner.nextLong();
+            Arrays.sort(arr);
+
+           if(n==1 && arr[0]>1)
+           System.out.println("NO");
+           else if(n==1 && arr[0]==1)
+           System.out.println("YES");
+           else if(arr[n-1]==arr[n-2] || arr[n-1]==arr[n-2]+1)
+           System.out.println("YES");
+           else
+           System.out.println("NO");
+        }
     }
 }
